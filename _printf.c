8f4f27ca4532c7def8c55c02144feda8a84e9217
Hanoui <slayeroma@hotmail.com>
2023-07-24 14:18:19 -0700

    First t

--- _printf.c
+++ _printf.c
@@ -10,53 +10,53 @@
  */
 int _printf(const char *format, ...)
 {
-    int i, count = 0;
-    char ch;
-    char *str;
-    va_list args;
+	int i, count = 0;
+	char ch;
+	char *str;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    ch = (char)va_arg(args, int);
-                    putchar(ch);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[i]);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(format[i]);
-            count++;
-        }
-    }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					ch = (char)va_arg(args, int);
+					putchar(ch);
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return (count);
 }
